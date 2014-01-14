89d23377573d601fa48b0c930145b23d96b2fc84
Bence Kalmar <bkalmar1996@gmail.com>
2014-01-14 09:05:46 +0100

    Clean up init.el.  Move system-specific init files.
    Remove custom-set-variables from init.el.  Move init-gnu-linux.el and
    init-windows-nt.el into their own directories.  These directories might
    contain other system-specific files in the future.

--- init.el
+++ init.el
@@ -6,22 +6,10 @@
 (cond
  ((equal system-type 'gnu/linux)
   (message "Loading GNU/Linux specific init file...")
-  (load-file "~/.emacs.d/init-gnu-linux.el"))
+  (load-file "~/.emacs.d/gnu-linux/init.el"))
  ((equal system-type 'windows-nt)
   (message "Loading Windows NT specific init file...")
-  (load-file "~/.emacs.d/init-windows-nt.el"))
+  (load-file "~/.emacs.d/windows-nt/init.el"))
  (t
   (message "Could not find appropriate config file for system type: %s"
            system-type)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("e4cac8bc0265e17d08a8ad4629a6c95ab751dfceec3c55110bc76aaf954962ed" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
