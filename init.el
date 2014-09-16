5e4b664cac67b329dc178beef8908296ae91ccc4
Bence Kalmar <bkalmar1996@gmail.com>
2014-09-16 16:13:40 +0200

    Customize calendar to better date & week format.

--- init.el
+++ init.el
@@ -416,6 +416,24 @@ If NOT-ABS is non-nil, do not prefix the string if it's an absolute path."
 ;;; haskell-mode
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 
+;;; calendar
+(setq calendar-date-style 'iso)
+
+;; start week with Monday
+(setq calendar-week-start-day 1)
+
+;; ISO week numbers
+(defface init-calendar-iso-week
+  '((t . (:height 0.9 :inherit font-lock-builtin-face)))
+  "Face used to highlight ISO week numbers in calendar.")
+
+(setq calendar-intermonth-text
+      '(propertize
+        (format "%2d" (car (calendar-iso-from-absolute
+                           (calendar-absolute-from-gregorian
+                            (list month day year)))))
+        'font-lock-face 'init-calendar-iso-week))
+
 ;;;; Useful modes for programming mode hooks
 
 ;; NOTE: fci-mode temporarily removed because of incompatibility with
