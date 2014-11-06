d6bc2f640b1a57902b4f0f3ac744b5d671235a88
Bence Kalmar <bkalmar1996@gmail.com>
2014-11-06 18:23:36 +0100

    Update obsolete MELPA repository URL to new URL.

--- init-packages.el
+++ init-packages.el
@@ -9,7 +9,7 @@
 
 ;;; Archives
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 
 ;;;; Initialize all ELPA packages.
 
