62afbebb14efec19f29ffdbc420d9fc29a501f90
Bence Kalmar <bkalmar1996@gmail.com>
2015-03-28 20:35:35 +0100

    Add preferred fonts for GNU/Linux.

--- gnu-linux/init.el
+++ gnu-linux/init.el
@@ -40,3 +40,13 @@ be integrated."
 
 ;; integrate clipboard
 (init-integrate-clipboard)
+
+;;;; Themes
+
+(setq init-preferred-fonts-monospace
+      '("DejaVu Sans Mono" "Unifont" "VL Gothic" "Liberation Mono" "FreeMono"
+        "Andale Mono" "Droid Sans Mono"))
+
+(setq init-preferred-fonts-proportional
+      '("DejaVu Sans" "Arial" "VL PGothic" "Linux Biolinum O" "Droid Sans"
+        "FreeSans" "Trebuchet MS" "Lato" "Liberation Sans" "Carlito"))
