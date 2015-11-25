4af74f980a0efe9206d6707f43ed756c231363a5
Bence Kalmar <bkalmar1996@gmail.com>
2015-11-25 15:15:47 +0100

    Add more backends for company.

--- elisp/packages/company.el
+++ elisp/packages/company.el
@@ -15,6 +15,15 @@
 (require 'company-c-headers)
 (add-to-list 'company-backends 'company-c-headers)
 
+(require 'company-ghci)
+(add-to-list 'company-backends 'company-ghci)
+
+(require 'company-jedi)
+(add-to-list 'company-backends 'company-jedi)
+
+(require 'company-math)
+(add-to-list 'company-backends 'company-math)
+
 (require 'company-web-html)
 (add-to-list 'company-backends 'company-web-html)
 
