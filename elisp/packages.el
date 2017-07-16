f619e5b36e4785e7a3ad20a6c33e46b86429e0c5
Bence Kalmar <bkalmar1996@gmail.com>
2017-07-16 03:24:00 +0200

    Add ability to use multiple cursors.

--- elisp/packages.el
+++ elisp/packages.el
@@ -42,6 +42,7 @@
    (json-mode             . "melpa")
    (lua-mode              . "melpa")
    (markdown-mode         . "melpa")
+   (multiple-cursors      . "melpa")
    (nhexl-mode            . "gnu")
    (org                   . "melpa")
    (palette               . "melpa")
