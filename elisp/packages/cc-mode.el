05f9d64769b0704cb249cd36a4503ddc0b2848e1
Bence Kalmar <bkalmar1996@gmail.com>
2017-08-23 01:04:52 +0200

    Use TABs for indenting C.

--- elisp/packages/cc-mode.el
+++ elisp/packages/cc-mode.el
@@ -1,3 +1,8 @@
 (require 'cc-mode)
 
 (customize-set-variable 'c-basic-offset 4)
+
+(add-to-list 'c-default-style '(c-mode . "k&r"))
+
+(add-hook 'c-mode-hook
+          (lambda () (setq indent-tabs-mode t) (setq tab-width 4)) t)
