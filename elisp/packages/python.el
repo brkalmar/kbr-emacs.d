ec674a541e310d8cb27ece576086c74546ad2bcf
Bence Kalmar <bkalmar1996@gmail.com>
2018-12-05 14:41:08 +0100

    feat(python): better built-in shell

--- elisp/packages/python.el
+++ elisp/packages/python.el
@@ -1,3 +1,7 @@
 (require 'python)
 
-(customize-set-variable 'python-shell-interpreter "python3")
+(customize-set-variable 'python-shell-interpreter "ipython3")
+(customize-set-variable 'python-shell-interpreter-args
+                        "-i --simple-prompt")
+(customize-set-variable 'python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters "ipython3")
