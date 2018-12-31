cc10b264b4f4e475382c8d057afdba286ec62558
Bence Kalmar <bkalmar1996@gmail.com>
2018-12-31 17:23:56 +0100

    fix(glasses): use capital-highlighting instead of underscores

--- elisp/packages/glasses-mode.el
+++ elisp/packages/glasses-mode.el
@@ -1,3 +1,13 @@
 (require 'glasses)
+(require 'zenburn-theme)
 
+(customize-set-variable 'glasses-original-separator "")
 (customize-set-variable 'glasses-separate-parentheses-p nil)
+(customize-set-variable 'glasses-separator "")
+
+(zenburn-with-color-variables
+  (defface bkalmar/glasses-face
+    `((t . (:underline ,zenburn-bg+3 :weight bold)))
+    "Highlights captitals in `glasses-mode'."))
+
+(customize-set-variable 'glasses-face 'bkalmar/glasses-face)
