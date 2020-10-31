fe39f64492a799ceff2a5139960aeed1764b1e1c
Bence Kalmar <bkalmar1996@gmail.com>
2020-11-01 00:12:54 +0100

    fix(themes): set preferred fonts only if font list is available

--- elisp/themes.el
+++ elisp/themes.el
@@ -7,29 +7,40 @@
 (load-theme 'zenburn t)
 (load-theme 'bkalmar t)
 
-;; default & fixed-pitch font
-
 (defvar bkalmar/preferred-fonts-monospace nil
   "List of preferred monospace fonts, in descending order of preference.
 
 Should be set in OS-specific files.")
-
-(catch 'break
-  (dolist (font-family bkalmar/preferred-fonts-monospace)
-    (when (member font-family (font-family-list))
-      (set-face-attribute 'default nil :family font-family)
-      (set-face-attribute 'fixed-pitch nil :family font-family)
-      (throw 'break t))))
-
-;; variable-pitch font
 
 (defvar bkalmar/preferred-fonts-proportional nil
   "List of preferred proportional fonts, in descending order of preference.
 
 Should be set in OS-specific files.")
 
-(catch 'break
-  (dolist (font-family bkalmar/preferred-fonts-proportional nil)
-    (when (member font-family (font-family-list))
-      (set-face-attribute 'variable-pitch nil :family font-family)
-      (throw 'break t))))
+(defvar bkalmar/preferred-fonts-set nil
+  "Keeps track of whether the preferred fonts had been set already.")
+
+(defun bkalmar/preferred-fonts-set (new-frame)
+  "Set current and default fonts to the preferred fonts for every frame.
+
+Does so only if `bkalmar/preferred-fonts-set' is nil and the font family list is
+available; then sets `bkalmar/preferred-fonts-set' to non-nil.
+
+The `new-frame' argument is ignored."
+  (when (and (null bkalmar/preferred-fonts-set) (font-family-list))
+    ;; default & fixed-pitch font
+    (catch 'break
+      (dolist (font-family bkalmar/preferred-fonts-monospace)
+        (when (member font-family (font-family-list))
+          (set-face-attribute 'default nil :family font-family)
+          (set-face-attribute 'fixed-pitch nil :family font-family)
+          (throw 'break t))))
+    ;; variable-pitch font
+    (catch 'break
+      (dolist (font-family bkalmar/preferred-fonts-proportional)
+        (when (member font-family (font-family-list))
+          (set-face-attribute 'variable-pitch nil :family font-family)
+          (throw 'break t))))
+    (setq bkalmar/preferred-fonts-set t)))
+
+(add-hook 'after-make-frame-functions 'bkalmar/preferred-fonts-set t)
