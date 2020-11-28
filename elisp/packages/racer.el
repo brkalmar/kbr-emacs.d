3bac4a927a611a89adab0ca0731069bd5e1a5095
Bence Kalmar <bkalmar1996@gmail.com>
2020-11-28 21:42:10 +0100

    fix(rust): add workarounds

--- elisp/packages/racer.el
+++ elisp/packages/racer.el
@@ -1,6 +1,14 @@
+(require 'f)
 (require 'racer)
 (require 'rust-mode)
 
 (add-hook 'rust-mode-hook #'racer-mode)
 
 (define-key racer-mode-map (kbd "C-c d") 'racer-describe)
+
+;; NOTE: Workaround until emacs-racer is updated to recognize the new directory
+;; layout.
+(customize-set-variable
+ 'racer-rust-src-path
+ (f-join "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu"
+         "lib/rustlib/src/rust/library"))
--- elisp/packages/rust-mode.el
+++ elisp/packages/rust-mode.el
@@ -1,5 +1,11 @@
 (require 'rust-mode)
 
+;; NOTE: We always use stable rustfmt for now, as it is not available on some
+;; nightly toolchains.
+(customize-set-variable
+ 'rust-rustfmt-switches
+ (cons "+stable" rust-rustfmt-switches))
+
 (customize-set-variable 'rust-format-on-save t)
 
 (add-hook 'rust-mode-hook
