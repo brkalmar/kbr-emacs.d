a82270e79ac606aca2f693cde7f3e1812c3601ea
Bence Kalmar <bkalmar1996@gmail.com>
2021-03-07 16:50:19 +0100

    refactor: remove unused package customization files

--- elisp/packages/x-win.el
+++
@@ -1,6 +0,0 @@
-(require 'x-win)
-
-(defun emacs-session-filename (session-id)
-  (let ((dir (concat bkalmar/emacs-config-directory "x-win/")))
-    (mkdir dir t)   
-    (expand-file-name (concat dir "session." session-id))))
